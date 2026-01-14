49e53dd81122218ab4ca8a39c32c7735b411d2aa
Dynnho <dynnho23@hotmail.com>
2026-01-14 14:23:46 -0300

    feat: initialize GitHub Finder project with React, Vite, and TypeScript
    - Add package.json with dependencies and scripts for development
    - Create Vite logo SVG file
    - Set up basic CSS files for styling
    - Implement main App component with routing
    - Create reusable components: BackBtn, Loader, Repo, Search, User, and Error
    - Develop Home and Repos routes for user and repository display
    - Define TypeScript types for user and repository data
    - Configure TypeScript with appropriate settings for app and node
    - Set up Vite configuration for React

---
+++ PROJETO_GITHUB_FINDER/github_finder/src/routes/Repos.tsx
@@ -0,0 +1,64 @@
+import type { RepoPros } from "../types/repo";
+
+import { useState, useEffect } from "react";
+
+import { useParams } from "react-router-dom";
+
+import BackBtn from "../components/BackBtn";
+
+import Loader from "../components/Loader";
+
+import Repo from "../components/Repo";
+
+import classes from "./Repos.module.css";
+
+const Repos = () => {
+  const { username } = useParams();
+
+  const [repos, setRepos] = useState<RepoPros[] | [] | null>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const loadRepos = async function (username: string) {
+      setIsLoading(true);
+
+      const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+      const data = await res.json();
+
+      setIsLoading(false);
+
+      let orderedRepos = data.sort(
+        (a: RepoPros, b: RepoPros) => b.stargazers_count - a.stargazers_count
+      );
+
+      orderedRepos = orderedRepos.slice(0, 5);
+
+      setRepos(orderedRepos);
+    };
+
+    if (username) {
+      loadRepos(username);
+    }
+  }, []);
+
+  if (!repos && isLoading) return <Loader />;
+
+  return (
+    <div className={classes.repos}>
+      <BackBtn />
+      <h2>Explore os repositórios do usuário: {username}</h2>
+      {repos && repos.length === 0 && <p>Não há repositários.</p>}
+      {repos && repos.length > 0 && (
+        <div className={classes.repos_container}>
+          {repos.map((repo: RepoPros) => (
+            <Repo key={repo.name} {...repo} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Repos;
